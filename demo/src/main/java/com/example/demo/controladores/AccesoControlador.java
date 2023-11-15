e71d1e8a85cac5577e5ff6cc9b9bc64aa4aff4a6
Danny <93393615+d-d4nny@users.noreply.github.com>
2023-11-16 00:17:53 +0100

    first commit

---
+++ demo/src/main/java/com/example/demo/controladores/AccesoControlador.java
@@ -0,0 +1,36 @@
+package com.example.demo.controladores;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.daos.Acceso;
+import com.example.demo.servicios.AccesoServicio;
+
+@RestController
+@RequestMapping("/accesos")
+public class AccesoControlador {
+
+	// Autowired Inyecta la dependencia instanciada lista para usar
+	@Autowired
+	private AccesoServicio accesoServicio;
+	
+	// GetMapping Define el endpoint al navegar para obtener todos los usuarios (http://localhost/accesos/todos)
+	@GetMapping("/todos")
+	public ArrayList<Acceso> obtenerTodos(){
+		return (ArrayList<Acceso>) accesoServicio.obtenerTodos();
+	}
+	
+	// La anotación @PathVariable mapea una parte de la URL de una
+	// solicitud HTTP a un parámetro de un método en un controlador en este caso el
+	// id (http://localhost/accesos/id/elidquesea)
+	@GetMapping("/id/{id}")
+	public Optional<Acceso> obtenerPorID(@PathVariable("id") long id){
+		return accesoServicio.obtenerPorId(id);
+	}
+}
